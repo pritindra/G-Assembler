919a5897980c018e1716db2e48044d1ea9e797e6
Pritindra <pritindra.d@gmail.com>
2021-08-09 18:56:12 +0530

    added conditions

--- gassembler.py
+++ gassembler.py
@@ -22,16 +22,91 @@ parser(sys.argv[1])
 def assemble():
     with open(sys.argv[1]) as fn:
         for line in fn:
-            tk = Tokenizer(line)
-            # label logic
-            if tk.curTok() == ".":
-                #logic
+            tk = tok.Tokenizer(line)
+            # comments 
+            if tk.curTok() == "#":
+                continue
             
-            # instructions logic
-            if tk.curTok() == "LD" or "ld":
-                #load logic
-                tk.advance()
-            if tk.curTok() == "ADD" or "add":
-                tk.advance()
+            # label logici
+            if tk.curTok() == ".":
+                continue    
 
-            #similar advances
+            # instructions logic
+            if tk.curValue() == "LD" or "ld":
+                r = int(tk.peekValue(1).upper()[1])
+                tk.advance(2)
+                if r>=0 and r<=7:
+                    a1 = tk.curValue()
+                    if a1 = "$": # address
+                        addr = int(a1[1:], 0)
+                        b = [0x02, r, addr >> 8, addr & 0xFF]
+                        writebin("out.bin", b)
+                    elif a1[0] == "R": # Register
+                        r2 = int(a1[1:0], 0)
+                        b = [0x01, r, 0, r2]
+                        writebin("out.bin", b)
+                    else: # Value
+                        v = int(a1,0)
+                        b = [0x00, r, v >> 8, v & 0xFF]
+                        writebin("out.bin", b)
+                else:
+                    print("Invalid identities")
+                    sys.exit()
+
+            if tk.curValue() == "ST" or "st":
+                
+                r = int(tk.peekValue(1).upper()[1])
+                tk.advance(2)
+                if r>=0 and r<=7:
+                    a1 = tk.curValue()
+                    if a1 = "$": # address
+                        addr = int(a1[1:], 0)
+                        b = [0x10, r, addr >> 8, addr & 0xFF]
+                        writebin("out.bin", b)
+                    elif a1[0] == "R": # Register
+                        r2 = int(a1[1:0], 0)
+                        b = [0x13, r, 0, r2]
+                        writebin("out.bin", b)
+                    else: # Value
+                        v = int(a1,0)
+                        b = [0x14, r, v >> 8, v & 0xFF]
+                        writebin("out.bin", b)
+                else:
+                    print("Invalid identities")
+                    sys.exit()
+
+            if tk.curValue() == "CMP" or "cmp":
+                
+                r = int(tk.peekValue(1).upper()[1])
+                tk.advance(2)
+                if r>=0 and r<=7:
+                    a1 = tk.curValue()
+                    if a1[0] == "R": # Register
+                        r2 = int(a1[1:0], 0)
+                        b = [0x20, r, 0, r2]
+                        writebin("out.bin", b)
+                    else: # Value
+                        v = int(a1,0)
+                        b = [0x21, r, v >> 8, v & 0xFF]
+                        writebin("out.bin", b)
+                else:
+                    print("Invalid identities")
+                    sys.exit()
+
+            if tk.curTok() == "ADD" or "add":
+                
+                r = int(tk.peekValue(1).upper()[1])
+                tk.advance(2)
+                if r>=0 and r<=7:
+                    a1 = tk.curValue()
+                    if a1[0] == "R": # Register
+                        r2 = int(a1[1:0], 0)
+                        b = [0x42, r, 0, r2]
+                        writebin("out.bin", b)
+                    else: # Value
+                        v = int(a1,0)
+                        b = [0x40, r, v >> 8, v & 0xFF]
+                        writebin("out.bin", b)
+                else:
+                    print("Invalid identities")
+                    sys.exit()
